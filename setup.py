72158bf6345b97a3d14db33bad7632627baf5101
Santiago Basanes <tiagobasa@gmail.com>
2023-11-06 10:29:09 -0300

    Added: Install files

---
+++ setup.py
@@ -0,0 +1,13 @@
+setup(
+    name="mycopernicus",
+    version="1.0.0",
+    description="",
+    author="Santiago Basañes",
+    author_email='santi.basanes@gmail.com',
+    url='https://github.com/tiagobasagh/mycopernicus',
+    packages=find_packages(include=["mycopernicus", "mymarinecopernicus", "mycopernicus.*"]),
+    install_requires=[
+        "pydap",
+        "xarray", 
+    ],
+    setup_requires=["flake8", "black"],
